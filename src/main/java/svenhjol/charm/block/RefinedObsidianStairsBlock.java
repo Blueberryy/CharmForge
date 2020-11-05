b2d31f5567b966ea17871ca76735a3067049b8a5
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:30:39 +0000

    Moar refined obsidian things

--- src/main/java/svenhjol/charm/block/RefinedObsidianStairsBlock.java
+++ src/main/java/svenhjol/charm/block/RefinedObsidianStairsBlock.java
@@ -10,7 +10,7 @@ public class RefinedObsidianStairsBlock extends StairsBlock implements ICharmBlo
     private CharmModule module;
 
     public RefinedObsidianStairsBlock(CharmModule module) {
-        super(RefinedObsidian.REFINED_OBSIDIAN.getDefaultState(), AbstractBlock.Properties.copy(RefinedObsidian.REFINED_OBSIDIAN));
+        super(RefinedObsidian.REFINED_OBSIDIAN.getDefaultState(), AbstractBlock.Properties.from(RefinedObsidian.REFINED_OBSIDIAN));
         this.register(module, "refined_obsidian_stairs");
         this.module = module;
     }
