e7c4c5658dcdfb3b3b9b280bc311898a2d273dab
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:35:57 +0000

    VariantTrappedChestBlock

--- src/main/java/svenhjol/charm/block/VariantTrappedChestBlock.java
+++ src/main/java/svenhjol/charm/block/VariantTrappedChestBlock.java
@@ -3,24 +3,24 @@ package svenhjol.charm.block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ChestBlock;
-import net.minecraft.block.entity.TileEntity;
-import net.minecraft.block.entity.ChestTileEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stat.Stat;
-import net.minecraft.stat.Stats;
-import net.minecraft.text.TranslationTextComponent;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
+import net.minecraft.tileentity.ChestTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.collection.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
-import svenhjol.charm.TileEntity.VariantTrappedChestTileEntity;
-import svenhjol.charm.module.VariantChests;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.block.ICharmBlock;
 import svenhjol.charm.base.enums.IVariantMaterial;
+import svenhjol.charm.module.VariantChests;
+import svenhjol.charm.tileentity.VariantTrappedChestTileEntity;
 
 import javax.annotation.Nullable;
 
@@ -30,12 +30,12 @@ public class VariantTrappedChestBlock extends ChestBlock implements ICharmBlock,
     private final IVariantMaterial type;
 
     public VariantTrappedChestBlock(CharmModule module, IVariantMaterial type) {
-        super(Settings.copy(Blocks.TRAPPED_CHEST), () -> VariantChests.TRAPPED_BLOCK_ENTITY);
+        super(Properties.from(Blocks.TRAPPED_CHEST), () -> VariantChests.TRAPPED_BLOCK_ENTITY);
 
         this.module = module;
         this.type = type;
 
-        this.register(module, type.asString() + "_trapped_chest");
+        this.register(module, type.getString() + "_trapped_chest");
     }
 
     @Override
@@ -56,20 +56,20 @@ public class VariantTrappedChestBlock extends ChestBlock implements ICharmBlock,
 
     @Nullable
     @Override
-    public TileEntity createTileEntity(IBlockReader worldIn) {
+    public TileEntity createNewTileEntity(IBlockReader worldIn) {
         VariantTrappedChestTileEntity chest = new VariantTrappedChestTileEntity();
-        chest.setCustomName(new TranslationTextComponent("block." + module.mod + "." + type.asString() + "_trapped_chest"));
+        chest.setCustomName(new TranslationTextComponent("block." + module.mod + "." + type.getString() + "_trapped_chest"));
 
         return chest;
     }
 
     @Override
     public int getWeakPower(BlockState state, IBlockReader world, BlockPos pos, Direction direction) {
-        return MathHelper.clamp(ChestTileEntity.getPlayersLookingInChestCount(world, pos), 0, 15);
+        return MathHelper.clamp(ChestTileEntity.getPlayersUsing(world, pos), 0, 15);
     }
 
     @Override
-    public int getStrongRedstonePower(BlockState state, IBlockReader world, BlockPos pos, Direction direction) {
+    public int getStrongPower(BlockState state, IBlockReader world, BlockPos pos, Direction direction) {
         return direction == Direction.UP ? state.getWeakPower(world, pos, direction) : 0;
     }
 
@@ -87,6 +87,6 @@ public class VariantTrappedChestBlock extends ChestBlock implements ICharmBlock,
      * Copypasta from {@link net.minecraft.block.TrappedChestBlock}
      */
     protected Stat<ResourceLocation> getOpenStat() {
-        return Stats.CUSTOM.getOrCreateStat(Stats.TRIGGER_TRAPPED_CHEST);
+        return Stats.CUSTOM.get(Stats.TRIGGER_TRAPPED_CHEST);
     }
 }
