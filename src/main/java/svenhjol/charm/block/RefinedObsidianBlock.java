a80467ecfdc91d889495963796d67fbe0165b144
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:30:18 +0000

    RefinedObsidianBlock

--- src/main/java/svenhjol/charm/block/RefinedObsidianBlock.java
+++ src/main/java/svenhjol/charm/block/RefinedObsidianBlock.java
@@ -2,14 +2,14 @@ package svenhjol.charm.block;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
-import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.block.SoundType;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.block.CharmBlock;
 
 public class RefinedObsidianBlock extends CharmBlock {
     public RefinedObsidianBlock(CharmModule module) {
         super(module, "refined_obsidian", AbstractBlock.Properties
-            .copy(Blocks.OBSIDIAN)
+            .from(Blocks.OBSIDIAN)
             .sound(SoundType.BASALT)
         );
     }
