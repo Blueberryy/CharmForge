fbd6cbd9de6dd733204f21a5a18f6861c25dea46
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:32:40 +0000

    VariantLadderBlock

--- src/main/java/svenhjol/charm/block/VariantLadderBlock.java
+++ src/main/java/svenhjol/charm/block/VariantLadderBlock.java
@@ -5,7 +5,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.LadderBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.collection.NonNullList;
+import net.minecraft.util.NonNullList;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.block.ICharmBlock;
 import svenhjol.charm.base.enums.IVariantMaterial;
@@ -14,8 +14,8 @@ public class VariantLadderBlock extends LadderBlock implements ICharmBlock {
     private final CharmModule module;
 
     public VariantLadderBlock(CharmModule module, IVariantMaterial type) {
-        super(AbstractBlock.Properties.copy(Blocks.LADDER));
-        register(module, type.asString() + "_ladder");
+        super(AbstractBlock.Properties.from(Blocks.LADDER));
+        register(module, type.getString() + "_ladder");
 
         /** @see net.minecraft.block.entity.AbstractFurnaceTileEntity#createFuelTimeMap */
         this.setBurnTime(300);
