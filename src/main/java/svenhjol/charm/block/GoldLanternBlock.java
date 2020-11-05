47e12080ecdc23f955af83faac46008eb2665836
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:04:17 +0000

    GoldLanternBlock

--- src/main/java/svenhjol/charm/block/GoldLanternBlock.java
+++ src/main/java/svenhjol/charm/block/GoldLanternBlock.java
@@ -6,6 +6,6 @@ import svenhjol.charm.base.CharmModule;
 
 public class GoldLanternBlock extends BaseLanternBlock {
     public GoldLanternBlock(CharmModule module, String name) {
-        super(module, name, AbstractBlock.Settings.copy(Blocks.LANTERN));
+        super(module, name, AbstractBlock.Properties.from(Blocks.LANTERN));
     }
 }
