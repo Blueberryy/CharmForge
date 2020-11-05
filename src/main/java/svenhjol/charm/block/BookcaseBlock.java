d000d7f68d72af5cef1172c646bb1889d15ca73d
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 22:47:54 +0000

    Fixed up bookcase block

--- src/main/java/svenhjol/charm/block/BookcaseBlock.java
+++ src/main/java/svenhjol/charm/block/BookcaseBlock.java
@@ -4,15 +4,23 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.material.PushReaction;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
-import net.minecraft.util.ActionResult;
+import net.minecraft.state.StateContainer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import svenhjol.charm.TileEntity.BookcaseTileEntity;
 import svenhjol.charm.base.CharmModule;
@@ -28,8 +36,7 @@ public class BookcaseBlock extends CharmBlockWithEntity {
     protected IVariantMaterial type;
 
     public BookcaseBlock(CharmModule module, IVariantMaterial type) {
-        super(module, type.getString() + "_bookcase", AbstractBlock.Settings
-            .copy(Blocks.BOOKSHELF));
+        super(module, type.getString() + "_bookcase", AbstractBlock.Properties.from(Blocks.BOOKSHELF));
 
         this.module = module;
         this.type = type;
@@ -38,43 +45,47 @@ public class BookcaseBlock extends CharmBlockWithEntity {
     }
 
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
         if (!world.isRemote && !player.isSpectator()) {
 
-            TileEntity TileEntity = world.getTileEntity(pos);
-            if (TileEntity instanceof BookcaseTileEntity) {
-                BookcaseTileEntity bookcase = (BookcaseTileEntity)TileEntity;
-                bookcase.checkLootInteraction(player);
-                player.openHandledScreen(bookcase);
+            TileEntity tileEntity = world.getTileEntity(pos);
+            if (tileEntity instanceof BookcaseTileEntity) {
+                BookcaseTileEntity bookcase = (BookcaseTileEntity)tileEntity;
+                bookcase.fillWithLoot(player);
+                player.openContainer(bookcase);
             }
         }
-        return ActionResult.SUCCESS;
+        return ActionResultType.SUCCESS;
     }
 
-
     @Override
-    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+    public void onReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         if (state.getBlock() != newState.getBlock()) {
             TileEntity tile = world.getTileEntity(pos);
             if (tile instanceof BookcaseTileEntity) {
-                ItemScatterer.spawn(world, pos, (Inventory) tile);
-                world.updateComparators(pos, this);
+                InventoryHelper.dropInventoryItems(world, pos, (IInventory)tile);
+                world.updateComparatorOutputLevel(pos, this);
             }
         }
     }
 
     @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
-        if (stack.hasCustomName()) {
+    public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        if (stack.hasDisplayName()) {
             TileEntity tile = world.getTileEntity(pos);
             if (tile instanceof BookcaseTileEntity)
-                ((BookcaseTileEntity) tile).setCustomName(stack.getName());
+                ((BookcaseTileEntity) tile).setCustomName(stack.getDisplayName());
         }
     }
 
     @Override
-    public PistonBehavior getPistonBehavior(BlockState state) {
-        return PistonBehavior.NORMAL;
+    public PushReaction getPushReaction(BlockState state) {
+        return PushReaction.NORMAL;
+    }
+
+    @Override
+    public float getEnchantPowerBonus(BlockState state, IWorldReader world, BlockPos pos) {
+        return state.get(SLOTS) > 0 ? 1 : 0;
     }
 
     @Override
@@ -83,20 +94,20 @@ public class BookcaseBlock extends CharmBlockWithEntity {
     }
 
     @Override
-    public boolean hasComparatorOutput(BlockState state) {
+    public boolean hasComparatorInputOverride(BlockState state) {
         return true;
     }
 
     @Override
-    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+    public int getComparatorInputOverride(BlockState state, World world, BlockPos pos) {
         return state.get(SLOTS);
     }
 
     @Nullable
     @Override
-    public TileEntity createTileEntity(BlockView world) {
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         BookcaseTileEntity bookcase = new BookcaseTileEntity();
-        bookcase.setCustomName(new TranslatableText("block." + module.mod + "." + type.getString() + "_bookcase"));
+        bookcase.setCustomName(new TranslationTextComponent("block." + module.mod + "." + type.getString() + "_bookcase"));
         return bookcase;
     }
 
@@ -106,8 +117,8 @@ public class BookcaseBlock extends CharmBlockWithEntity {
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        super.appendProperties(builder);
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
         builder.add(SLOTS);
     }
 }
