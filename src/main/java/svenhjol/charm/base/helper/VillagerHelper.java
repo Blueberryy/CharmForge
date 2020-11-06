efa67ba1007e999bd43576c8b78e69ff5d36e098
Sven Hjól <sven.hjol@protonmail.com>
2020-11-06 20:06:37 +0000

    Restore villager helper methods

--- src/main/java/svenhjol/charm/base/helper/VillagerHelper.java
+++ src/main/java/svenhjol/charm/base/helper/VillagerHelper.java
@@ -1,16 +1,37 @@
 package svenhjol.charm.base.helper;
 
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.entity.merchant.villager.VillagerTrades;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.MerchantOffer;
 import net.minecraft.util.IItemProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.village.PointOfInterestType;
+import svenhjol.charm.base.handler.RegistryHandler;
+import svenhjol.charm.mixin.accessor.PointOfInterestTypeAccessor;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 public class VillagerHelper {
+    public static VillagerProfession addProfession(ResourceLocation id, PointOfInterestType poit, SoundEvent worksound) {
+        VillagerProfession profession = new VillagerProfession(id.toString(), poit, ImmutableSet.of(), ImmutableSet.of(), worksound);
+        VillagerProfession registeredProfession = RegistryHandler.villagerProfession(id, profession);
+//        PROFESSION_TO_LEVELED_TRADE.put(profession, new Int2ObjectOpenHashMap<>());
+        return registeredProfession;
+    }
+
+    public static PointOfInterestType addPointOfInterestType(ResourceLocation id, Block block, int ticketCount) {
+        PointOfInterestType poit = new PointOfInterestType(id.toString(), ImmutableSet.copyOf(block.getStateContainer().getValidStates()), ticketCount, 1);
+        RegistryHandler.pointOfInterestType(id, poit);
+        return PointOfInterestTypeAccessor.invokeRegisterBlockStates(poit);
+    }
+
     public static abstract class SingleItemTypeTrade implements VillagerTrades.ITrade {
         protected IItemProvider in = Items.AIR;
         protected IItemProvider out = Items.EMERALD;
--- src/main/java/svenhjol/charm/mixin/accessor/PointOfInterestTypeAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/PointOfInterestTypeAccessor.java
@@ -3,9 +3,15 @@ package svenhjol.charm.mixin.accessor;
 import net.minecraft.village.PointOfInterestType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(PointOfInterestType.class)
 public interface PointOfInterestTypeAccessor {
     @Accessor
     void setMaxFreeTickets(int ticketCount);
+
+    @Invoker("registerBlockStates")
+    static PointOfInterestType invokeRegisterBlockStates(PointOfInterestType pointOfInterestType) {
+        throw new IllegalStateException();
+    }
 }
