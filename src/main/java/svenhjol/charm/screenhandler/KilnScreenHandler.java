a44fbcdd6c19aa8797fc6e6162887f67f0d892f8
Sven Hjól <sven.hjol@protonmail.com>
2020-11-06 00:24:54 +0000

    Fix up Kilns

--- src/main/java/svenhjol/charm/screenhandler/KilnScreenHandler.java
+++ src/main/java/svenhjol/charm/screenhandler/KilnScreenHandler.java
@@ -1,18 +1,18 @@
 package svenhjol.charm.screenhandler;
 
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.recipe.book.RecipeBookCategory;
-import net.minecraft.screen.AbstractFurnaceScreenHandler;
-import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.container.AbstractFurnaceContainer;
+import net.minecraft.item.crafting.RecipeBookCategory;
+import net.minecraft.util.IIntArray;
 import svenhjol.charm.module.Kilns;
 
-public class KilnScreenHandler extends AbstractFurnaceScreenHandler {
+public class KilnScreenHandler extends AbstractFurnaceContainer {
     public KilnScreenHandler(int syncId, PlayerInventory playerInventory) {
         super(Kilns.SCREEN_HANDLER, Kilns.RECIPE_TYPE, RecipeBookCategory.SMOKER, syncId, playerInventory);
     }
 
-    public KilnScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate propertyDelegate) {
+    public KilnScreenHandler(int syncId, PlayerInventory playerInventory, IInventory inventory, IIntArray propertyDelegate) {
         super(Kilns.SCREEN_HANDLER, Kilns.RECIPE_TYPE, RecipeBookCategory.SMOKER, syncId, playerInventory, inventory, propertyDelegate);
     }
 }
---
+++ src/main/java/svenhjol/charm/tileentity/KilnTileEntity.java
@@ -0,0 +1,31 @@
+package svenhjol.charm.tileentity;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.AbstractFurnaceTileEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import svenhjol.charm.module.Kilns;
+import svenhjol.charm.screenhandler.KilnScreenHandler;
+
+public class KilnTileEntity extends AbstractFurnaceTileEntity {
+    public KilnTileEntity() {
+        super(Kilns.BLOCK_ENTITY, Kilns.RECIPE_TYPE);
+    }
+
+    @Override
+    protected int getBurnTime(ItemStack fuel) {
+        return super.getBurnTime(fuel) / 2;
+    }
+
+    @Override
+    protected ITextComponent getDefaultName() {
+        return new TranslationTextComponent("container.charm.kiln");
+    }
+
+    @Override
+    protected Container createMenu(int id, PlayerInventory player) {
+        return new KilnScreenHandler(id, player, this, this.furnaceData);
+    }
+}
