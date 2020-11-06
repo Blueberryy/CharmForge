71afdcee65ec9c65b94a12b8d25441b970169c6b
Sven Hjól <sven.hjol@protonmail.com>
2020-11-06 20:56:06 +0000

    EndermitePowder

--- src/main/java/svenhjol/charm/module/EndermitePowder.java
+++ src/main/java/svenhjol/charm/module/EndermitePowder.java
@@ -1,18 +1,19 @@
 package svenhjol.charm.module;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.EndermiteEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.monster.EndermiteEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.handler.RegistryHandler;
@@ -23,7 +24,7 @@ import svenhjol.charm.client.EndermitePowderClient;
 import svenhjol.charm.entity.EndermitePowderEntity;
 import svenhjol.charm.item.EndermitePowderItem;
 
-@Module(mod = Charm.MOD_ID, description = "Endermites drop endermite powder that can be used to locate an End City.")
+@Module(mod = Charm.MOD_ID, description = "Endermites drop endermite powder that can be used to locate an End City.", hasSubscriptions = true)
 public class EndermitePowder extends CharmModule {
     public static ResourceLocation ID = new ResourceLocation(Charm.MOD_ID, "endermite_powder");
     public static EntityType<EndermitePowderEntity> ENTITY;
@@ -39,10 +40,10 @@ public class EndermitePowder extends CharmModule {
 
         // setup and register the entity
         // TODO "No data fixer registered for endermite_powder"
-        ENTITY = RegistryHandler.entity(ID, EntityType.Builder.<EndermitePowderEntity>create(EndermitePowderEntity::new, SpawnGroup.MISC)
-            .maxTrackingRange(80)
-            .trackingTickInterval(10)
-            .setDimensions(2.0F, 2.0F)
+        ENTITY = RegistryHandler.entity(ID, EntityType.Builder.<EndermitePowderEntity>create(EndermitePowderEntity::new, EntityClassification.MISC)
+            .trackingRange(80)
+            .setUpdateInterval(10)
+            .size(2.0F, 2.0F)
             .build(ID.getPath()));
     }
 
@@ -52,19 +53,18 @@ public class EndermitePowder extends CharmModule {
         new EndermitePowderClient(this);
     }
 
-    @Override
-    public void init() {
-        // react to entity drops
-        EntityDropsCallback.EVENT.register(this::tryDrop);
+    @SubscribeEvent
+    public void onLivingDrops(LivingDropsEvent event) {
+        if (!event.isCanceled())
+            tryDrop(event.getEntityLiving(), event.getSource(), event.getLootingLevel());
     }
 
-    private ActionResult tryDrop(Entity entity, DamageSource source, int lootingLevel) {
+    private void tryDrop(Entity entity, DamageSource source, int lootingLevel) {
         if (!entity.world.isRemote && entity instanceof EndermiteEntity) {
             World world = entity.getEntityWorld();
-            BlockPos pos = entity.getBlockPos();
-            int amount = ItemHelper.getAmountWithLooting(world.random, maxDrops, lootingLevel, (float)lootingBoost);
+            BlockPos pos = entity.getPosition();
+            int amount = ItemHelper.getAmountWithLooting(world.rand, maxDrops, lootingLevel, (float)lootingBoost);
             world.addEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ENDERMITE_POWDER, amount)));
         }
-        return ActionResult.PASS;
     }
 }
