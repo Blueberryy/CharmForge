64c74ede45f4e6f03e4cd7d4007ca084da25676f
Sven Hjól <sven.hjol@protonmail.com>
2020-11-06 14:50:39 +0000

    Added subscriptions annotation

--- src/main/java/svenhjol/charm/base/iface/Module.java
+++ src/main/java/svenhjol/charm/base/iface/Module.java
@@ -8,6 +8,7 @@ import java.lang.annotation.*;
 public @interface Module {
     boolean alwaysEnabled() default false;
     boolean enabledByDefault() default true;
+    boolean hasSubscriptions() default false;
     String description() default "";
     String mod() default "";
 }
