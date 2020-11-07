da2df4c7d065567556fb19f70a8e161638b74150
Sven Hjól <sven.hjol@protonmail.com>
2020-11-07 17:27:50 +0000

    PlaceableGlowstoneDust

--- src/main/java/svenhjol/charm/module/PlaceableGlowstoneDust.java
+++ src/main/java/svenhjol/charm/module/PlaceableGlowstoneDust.java
@@ -13,13 +13,15 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.helper.PosHelper;
 import svenhjol.charm.base.iface.Module;
 import svenhjol.charm.block.PlacedGlowstoneDustBlock;
 
-@Module(mod = Charm.MOD_ID, description = "Glowstone dust can be placed on the ground as a light source.")
+@Module(mod = Charm.MOD_ID, description = "Glowstone dust can be placed on the ground as a light source.", hasSubscriptions = true)
 public class PlaceableGlowstoneDust extends CharmModule {
     public static PlacedGlowstoneDustBlock PLACED_GLOWSTONE_DUST;
 
@@ -33,9 +35,12 @@ public class PlaceableGlowstoneDust extends CharmModule {
         RenderTypeLookup.setRenderLayer(PLACED_GLOWSTONE_DUST, RenderType.getCutout());
     }
 
-    @Override
-    public void init() {
-        UseBlockCallback.EVENT.register(this::tryPlaceDust);
+    @SubscribeEvent
+    public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (!event.isCanceled()) {
+            boolean result = tryPlaceDust(event.getPlayer(), event.getWorld(), event.getPos(), event.getFace(), event.getHand());
+            event.setCanceled(result);
+        }
     }
 
     public static boolean tryPlaceDust(World world, RayTraceResult hitResult) {
@@ -45,7 +50,13 @@ public class PlaceableGlowstoneDust extends CharmModule {
         BlockRayTraceResult BlockRayTraceResult = (BlockRayTraceResult)hitResult;
         BlockPos pos = BlockRayTraceResult.getPos();
         Direction side = BlockRayTraceResult.getFace();
+
+        return tryPlaceDust(world, pos, side);
+    }
+
+    public static boolean tryPlaceDust(World world, BlockPos pos, Direction side) {
         BlockState state = world.getBlockState(pos);
+
         BlockPos offsetPos = pos.offset(side);
 
         if (state.isSolidSide(world, pos, side) && PosHelper.isLikeAir(world, offsetPos) && world.getBlockState(offsetPos).getBlock() != Blocks.LAVA) {
@@ -64,25 +75,25 @@ public class PlaceableGlowstoneDust extends CharmModule {
         return false;
     }
 
-    private ActionResult tryPlaceDust(PlayerEntity player, World world, Hand hand, BlockRayTraceResult hitResult) {
+    private boolean tryPlaceDust(PlayerEntity player, World world, BlockPos pos, Direction side, Hand hand) {
         ItemStack stack = player.getHeldItem(hand);
 
         if (world != null && stack.getItem() == Items.GLOWSTONE_DUST) {
             player.swingArm(hand);
 
             if (!world.isRemote) {
-                boolean result = tryPlaceDust(world, hitResult);
+                boolean result = tryPlaceDust(world, pos, side);
 
                 if (result) {
                     if (!player.isCreative())
                         stack.shrink(1);
 
-                    return ActionResult.SUCCESS;
+                    return true;
                 }
-                return ActionResult.FAIL;
+                return false;
             }
         }
 
-        return ActionResult.PASS;
+        return false;
     }
 }
