4bd2f55901ba178934ddb062b287f1d09b0d1d5b
Sven Hjól <sven.hjol@protonmail.com>
2020-11-07 17:55:57 +0000

    Client for CoralSquids

--- src/main/java/svenhjol/charm/client/CoralSquidsClient.java
+++ src/main/java/svenhjol/charm/client/CoralSquidsClient.java
@@ -1,12 +1,12 @@
 package svenhjol.charm.client;
 
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import svenhjol.charm.module.CoralSquids;
 import svenhjol.charm.render.CoralSquidEntityRenderer;
 import svenhjol.charm.base.CharmModule;
 
 public class CoralSquidsClient {
     public CoralSquidsClient(CharmModule module) {
-        EntityRendererRegistry.INSTANCE.register(CoralSquids.CORAL_SQUID, (((dispatcher, context) ->
-            new CoralSquidEntityRenderer(dispatcher))));
+        RenderingRegistry.registerEntityRenderingHandler(CoralSquids.CORAL_SQUID, CoralSquidEntityRenderer::new);
     }
 }
