00b9f6a894e5a37482a4ddc34dc59e56246038fd
Sven Hjól <sven.hjol@protonmail.com>
2020-11-07 17:36:58 +0000

    RemoveNitwits

--- src/main/java/svenhjol/charm/module/RemoveNitwits.java
+++ src/main/java/svenhjol/charm/module/RemoveNitwits.java
@@ -4,19 +4,21 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.merchant.villager.VillagerData;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
-import net.minecraft.util.ActionResult;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.iface.Module;
 
-@Module(mod = Charm.MOD_ID, description = "When any action would cause a villager to become a nitwit, it becomes an unemployed villager instead.")
+@Module(mod = Charm.MOD_ID, description = "When any action would cause a villager to become a nitwit, it becomes an unemployed villager instead.", hasSubscriptions = true)
 public class RemoveNitwits extends CharmModule {
-    @Override
-    public void init() {
-        AddEntityCallback.EVENT.register(this::changeNitwitProfession);
+    @SubscribeEvent
+    public void onVillagerJoinWorld(EntityJoinWorldEvent event) {
+        if (!event.isCanceled())
+            changeNitwitProfession(event.getEntity());
     }
 
-    private ActionResult changeNitwitProfession(Entity entity) {
+    private void changeNitwitProfession(Entity entity) {
         if (!entity.world.isRemote
             && entity instanceof VillagerEntity
         ) {
@@ -28,7 +30,5 @@ public class RemoveNitwits extends CharmModule {
                 Charm.LOG.debug("Changed nitwit's profession to NONE: " + villager.getCachedUniqueIdString());
             }
         }
-
-        return ActionResult.PASS;
     }
 }
