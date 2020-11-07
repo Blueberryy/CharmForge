b2591b7adb783eeb97a5d8b15ea1d2c194b476a3
Sven Hjól <sven.hjol@protonmail.com>
2020-11-07 18:00:03 +0000

    Client for Glowballs. This is probably broken

--- src/main/java/svenhjol/charm/client/GlowBallsClient.java
+++ src/main/java/svenhjol/charm/client/GlowBallsClient.java
@@ -1,22 +1,13 @@
 package svenhjol.charm.client;
 
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.EntityType;
-import svenhjol.charm.entity.GlowBallEntity;
-import svenhjol.charm.event.ClientEntitySpawnCallback;
-import svenhjol.charm.module.GlowBalls;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.module.GlowBalls;
 
 public class GlowBallsClient {
     public GlowBallsClient(CharmModule module) {
-        EntityRendererRegistry.INSTANCE.register(GlowBalls.ENTITY, ((dispatcher, context)
-            -> new FlyingItemEntityRenderer(dispatcher, context.getItemRenderer())));
-    }
-
-    private void handleClientEntitySpawn(ClientWorld world, EntitySpawnS2CPacket packet, double x, double y, double z, EntityType<?> entityType) {
-        if (entityType == GlowBalls.ENTITY) {
-            GlowBallEntity entity = new GlowBallEntity(world, x, y, z);
-            ClientEntitySpawnCallback.addEntity(world, entity, packet, x, y, z);
-        }
+        RenderingRegistry.registerEntityRenderingHandler(GlowBalls.ENTITY, manager -> new SpriteRenderer<>(manager, Minecraft.getInstance().getItemRenderer()));
     }
 }
