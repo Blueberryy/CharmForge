47e7ebe7388bbacdf8444e9c0c10367b8265eeda
Sven Hjól <sven.hjol@protonmail.com>
2020-11-09 21:44:04 +0000

    Port ister stuff from 1.15

--- src/main/java/svenhjol/charm/base/block/ICharmBlock.java
+++ src/main/java/svenhjol/charm/base/block/ICharmBlock.java
@@ -2,13 +2,24 @@ package svenhjol.charm.base.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.*;
+import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.registries.ForgeRegistries;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.handler.RegistryHandler;
-import svenhjol.charm.mixin.accessor.*;
+import svenhjol.charm.mixin.accessor.FireBlockAccessor;
 
+import javax.annotation.Nullable;
+import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public interface ICharmBlock {
     boolean enabled();
@@ -16,7 +27,7 @@ public interface ICharmBlock {
     default void register(CharmModule module, String name) {
         ResourceLocation id = new ResourceLocation(module.mod, name);
         RegistryHandler.block(id, (Block)this);
-        createBlockItem(id);
+        RegistryHandler.register(ForgeRegistries.ITEMS, () -> createBlockItem(id));
     }
 
     default ItemGroup getItemGroup() {
@@ -29,7 +40,8 @@ public interface ICharmBlock {
         return 64;
     }
 
-    default void createBlockItem(ResourceLocation id) {
+    @Nullable
+    default BlockItem createBlockItem(ResourceLocation id) {
         Item.Properties settings = new Item.Properties();
 
         ItemGroup itemGroup = getItemGroup();
@@ -38,8 +50,15 @@ public interface ICharmBlock {
 
         settings.maxStackSize(getMaxStackSize());
 
-        CharmBlockItem blockItem = new CharmBlockItem(this, settings);
-        RegistryHandler.item(id, blockItem);
+        // set item stack renderer function if present
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            Supplier<Callable<ItemStackTileEntityRenderer>> ister = this.getISTER();
+
+            if (ister != null)
+                settings.setISTER(ister);
+        });
+
+        return new CharmBlockItem(this, settings);
     }
 
     default BiConsumer<ItemStack, Boolean> getInventoryTickConsumer() {
@@ -49,4 +68,9 @@ public interface ICharmBlock {
     default void setFireInfo(int encouragement, int flammability) {
         ((FireBlockAccessor) Blocks.FIRE).invokeSetFireInfo((Block)this, encouragement, flammability);
     }
+
+    @Nullable
+    @OnlyIn(Dist.CLIENT)
+    default Supplier<Callable<ItemStackTileEntityRenderer>> getISTER() { return null; }
+
 }
