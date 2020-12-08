5ecd4a35cca143e20605af8ac525eec5a5062dff
f43nd1r <lukas.morawietz@gmail.com>
2020-12-08 18:36:18 +0100

    Fix AutoRestock on composter

---
+++ src/main/java/svenhjol/charm/mixin/ComposterBlockMixin.java
@@ -0,0 +1,31 @@
+package svenhjol.charm.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ComposterBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(ComposterBlock.class)
+public class ComposterBlockMixin {
+
+    @Inject(
+            method = "onBlockActivated",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playEvent(ILnet/minecraft/util/math/BlockPos;I)V"),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    public void hookOnBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit,
+                                     CallbackInfoReturnable<ActionResultType> cir, int i, ItemStack itemstack, BlockState blockstate) {
+        player.addStat(Stats.ITEM_USED.get(itemstack.getItem()));
+    }
+}
