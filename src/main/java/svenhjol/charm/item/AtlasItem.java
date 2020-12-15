8a9a6b42dd0c2ffdeecda4102c36cf75975b96ed
f43nd1r <lukas.morawietz@gmail.com>
2020-12-15 02:29:59 +0100

    atlas right click on banner

--- src/main/java/svenhjol/charm/item/AtlasItem.java
+++ src/main/java/svenhjol/charm/item/AtlasItem.java
@@ -1,13 +1,17 @@
 package svenhjol.charm.item;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.minecraft.world.storage.MapData;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.helper.AtlasInventory;
 import svenhjol.charm.base.item.CharmItem;
 import svenhjol.charm.module.Atlas;
 
@@ -27,5 +31,30 @@ public class AtlasItem extends CharmItem {
         }
         playerIn.openContainer(Atlas.getInventory(worldIn, itemStack));
         return ActionResult.resultConsume(itemStack);
+    }
+
+    @Override
+    public ActionResultType onItemUse(ItemUseContext context) {
+        World world = context.getWorld();
+        BlockState blockstate = world.getBlockState(context.getPos());
+        if (blockstate.isIn(BlockTags.BANNERS)) {
+            if (!world.isRemote) {
+                PlayerEntity player = context.getPlayer();
+                if (player instanceof ServerPlayerEntity) {
+                    AtlasInventory inventory = Atlas.getInventory(world, context.getItem());
+                    AtlasInventory.MapInfo info = inventory.updateActiveMap((ServerPlayerEntity) player);
+                    if (info != null) {
+                        ItemStack map = inventory.getStackInSlot(info.slot);
+                        MapData mapdata = FilledMapItem.getMapData(map, context.getWorld());
+                        if (mapdata != null) {
+                            mapdata.tryAddBanner(context.getWorld(), context.getPos());
+                        }
+                    }
+                }
+            }
+            return ActionResultType.func_233537_a_(world.isRemote);
+        } else {
+            return super.onItemUse(context);
+        }
     }
 }
