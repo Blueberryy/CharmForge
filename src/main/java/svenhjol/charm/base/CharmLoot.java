42499fdbce72a9e5e98ddbc02b831c8d7538ea4e
Sven Hjól <sven.hjol@protonmail.com>
2020-12-18 16:38:54 +0000

    Register missing loot tables

--- src/main/java/svenhjol/charm/base/CharmLoot.java
+++ src/main/java/svenhjol/charm/base/CharmLoot.java
@@ -5,9 +5,13 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.base.helper.LootHelper;
 
 public class CharmLoot {
+    public static ResourceLocation VILLAGE_LUMBERJACK = new ResourceLocation(Charm.MOD_ID, "chests/village_lumberjack_house");
+    public static ResourceLocation VILLAGE_BEEKEEPER = new ResourceLocation(Charm.MOD_ID, "chests/village_beekeeper_house");
     public static ResourceLocation VILLAGE_LIBRARIAN = new ResourceLocation(Charm.MOD_ID, "chests/village_librarian");
 
     public static void init() {
+        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_LUMBERJACK);
+        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_BEEKEEPER);
         LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_LIBRARIAN);
     }
 }
