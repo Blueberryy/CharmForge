24956dda1541a2717882e20f1ad88d69fd1fdea9
Sven Hjól <sven.hjol@protonmail.com>
2020-12-20 01:16:09 +0000

    Wrong loot path

--- src/main/java/svenhjol/charm/base/CharmLoot.java
+++ src/main/java/svenhjol/charm/base/CharmLoot.java
@@ -5,9 +5,9 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.base.helper.LootHelper;
 
 public class CharmLoot {
-    public static ResourceLocation VILLAGE_LUMBERJACK = new ResourceLocation(Charm.MOD_ID, "chests/village_lumberjack_house");
-    public static ResourceLocation VILLAGE_BEEKEEPER = new ResourceLocation(Charm.MOD_ID, "chests/village_beekeeper_house");
-    public static ResourceLocation VILLAGE_LIBRARIAN = new ResourceLocation(Charm.MOD_ID, "chests/village_librarian");
+    public static ResourceLocation VILLAGE_LUMBERJACK = new ResourceLocation(Charm.MOD_ID, "chests/village/village_lumberjack_house");
+    public static ResourceLocation VILLAGE_BEEKEEPER = new ResourceLocation(Charm.MOD_ID, "chests/village/village_beekeeper_house");
+    public static ResourceLocation VILLAGE_LIBRARIAN = new ResourceLocation(Charm.MOD_ID, "chests/village/village_librarian");
 
     public static void init() {
         LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_LUMBERJACK);
