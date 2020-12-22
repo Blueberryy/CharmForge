5db3813180b42bf8e921a64a5ca8149c5f3fbdb8
Sven Hjól <sven.hjol@protonmail.com>
2020-12-22 23:36:02 +0000

    Port hoe harvesting fixes

--- src/main/java/svenhjol/charm/module/HoeHarvesting.java
+++ src/main/java/svenhjol/charm/module/HoeHarvesting.java
@@ -21,7 +21,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.handler.ModuleHandler;
-import svenhjol.charm.base.helper.ModHelper;
 import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
 
@@ -60,9 +59,7 @@ public class HoeHarvesting extends CharmModule {
     public boolean tryHarvest(PlayerEntity player, World world, Hand hand, BlockPos pos) {
         ItemStack held = player.getHeldItem(hand);
 
-        if (!world.isRemote && held.getItem() instanceof HoeItem) {
-            ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
-            ServerWorld serverWorld = (ServerWorld)serverPlayer.world;
+        if (held.getItem() instanceof HoeItem) {
             BlockState state = world.getBlockState(pos);
             Block block = state.getBlock();
 
@@ -72,21 +69,27 @@ public class HoeHarvesting extends CharmModule {
             Item blockItem = block.asItem();
             BlockState newState = block.getDefaultState();
 
-            List<ItemStack> drops = Block.getDrops(state, serverWorld, pos, null, player, ItemStack.EMPTY);
-            for (ItemStack drop : drops) {
-                if (drop.getItem() == blockItem)
-                    drop.shrink(1);
+            if (!world.isRemote) {
+                ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
+                ServerWorld serverWorld = (ServerWorld)serverPlayer.world;
 
-                if (!drop.isEmpty())
-                    Block.spawnAsEntity(world, pos, drop);
+                List<ItemStack> drops = Block.getDrops(state, serverWorld, pos, null, player, ItemStack.EMPTY);
+                for (ItemStack drop : drops) {
+                    if (drop.getItem() == blockItem)
+                        drop.shrink(1);
+
+                    if (!drop.isEmpty())
+                        Block.spawnAsEntity(world, pos, drop);
+                }
+
+                world.playEvent(2001, pos, Block.getStateId(newState));
+                world.setBlockState(pos, newState);
+                world.playSound(null, pos, SoundEvents.BLOCK_CROP_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+                // damage the hoe a bit
+                held.damageItem(1, player, p -> p.swingArm(hand));
             }
 
-            world.playEvent(2001, pos, Block.getStateId(newState));
-            world.setBlockState(pos, newState);
-            world.playSound(null, pos, SoundEvents.BLOCK_CROP_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
-
-            // damage the hoe a bit
-            held.damageItem(1, player, p -> p.swingArm(hand));
             return true;
         }
 
