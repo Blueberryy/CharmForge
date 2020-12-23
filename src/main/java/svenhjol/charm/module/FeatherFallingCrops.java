8524037a458ec65b61ea2d46355b5078249483c1
f43nd1r <lukas.morawietz@gmail.com>
2020-12-23 03:40:04 +0100

    featherless FeatherFallingCrops

--- src/main/java/svenhjol/charm/module/FeatherFallingCrops.java
+++ src/main/java/svenhjol/charm/module/FeatherFallingCrops.java
@@ -5,13 +5,18 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import svenhjol.charm.Charm;
-import svenhjol.charm.base.handler.ModuleHandler;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.handler.ModuleHandler;
 import svenhjol.charm.base.helper.EnchantmentsHelper;
+import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
 
 @Module(mod = Charm.MOD_ID, description = "A player wearing feather falling enchanted boots will not trample crops.", hasSubscriptions = true)
 public class FeatherFallingCrops extends CharmModule {
+
+    @Config(name = "Requires feather falling", description = "Turn this off to prevent trampling even when the player does not wear feather falling boots.")
+    public static boolean requiresFeatherFalling = true;
+
     @SubscribeEvent
     public void onFarmlandTrample(BlockEvent.FarmlandTrampleEvent event) {
         if (!event.isCanceled()) {
@@ -23,7 +28,7 @@ public class FeatherFallingCrops extends CharmModule {
 
     public boolean landedOnFarmlandBlock(Entity entity) {
         return ModuleHandler.enabled(FeatherFallingCrops.class)
-            && entity instanceof LivingEntity
-            && EnchantmentsHelper.hasFeatherFalling((LivingEntity)entity);
+                && entity instanceof LivingEntity
+                && (!requiresFeatherFalling || EnchantmentsHelper.hasFeatherFalling((LivingEntity) entity));
     }
 }
