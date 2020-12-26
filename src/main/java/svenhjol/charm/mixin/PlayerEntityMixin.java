c25e456faf1a23ff3c78ea9887d233cb30359548
f43nd1r <lukas.morawietz@gmail.com>
2020-12-26 04:07:14 +0100

    aerial affinity

--- src/main/java/svenhjol/charm/mixin/PlayerEntityMixin.java
+++ src/main/java/svenhjol/charm/mixin/PlayerEntityMixin.java
@@ -1,14 +1,18 @@
 package svenhjol.charm.mixin;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.module.AerialAffinity;
 import svenhjol.charm.module.ParrotsStayOnShoulder;
 
 @Mixin(PlayerEntity.class)
@@ -27,5 +31,11 @@ public abstract class PlayerEntityMixin extends Entity {
     private void hookSpawnShoulderEntities(CallbackInfo ci) {
         if (ParrotsStayOnShoulder.shouldParrotStayMounted(this.world, this.timeEntitySatOnShoulder))
             ci.cancel();
+    }
+
+    @Redirect(method = "getDigSpeed(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)F",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;onGround:Z"))
+    private boolean hookDigSpeedOnGround(PlayerEntity player, BlockState state, BlockPos pos) {
+        return player.isOnGround() || AerialAffinity.digFast(player);
     }
 }
