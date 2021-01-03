0cb738c3cc840492c4b66ab5d7b6436cdaf57840
f43nd1r <lukas.morawietz@gmail.com>
2021-01-03 23:13:50 +0100

    prevent take open atlas

--- src/main/java/svenhjol/charm/container/AtlasContainer.java
+++ src/main/java/svenhjol/charm/container/AtlasContainer.java
@@ -1,20 +1,22 @@
 package svenhjol.charm.container;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Slot;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.Hand;
 import svenhjol.charm.base.container.CharmContainer;
 import svenhjol.charm.base.container.ConditionalSlot;
+import svenhjol.charm.base.helper.ItemNBTHelper;
 import svenhjol.charm.module.Atlas;
 
+import java.util.Objects;
+
 public class AtlasContainer extends CharmContainer {
-    private final AtlasInventory inventory;
+    private final AtlasInventory atlasInventory;
 
     public AtlasContainer(int syncId, PlayerInventory player, AtlasInventory inventory) {
         super(Atlas.CONTAINER, syncId, player, inventory);
-        this.inventory = inventory;
+        this.atlasInventory = inventory;
         // container's inventory slots
         for (int r = 0; r < 3; ++r) {
             this.addSlot(new ConditionalSlot(stack -> stack.getItem() == Items.MAP, inventory, r, 8, 18 + r * 18));
@@ -30,11 +32,17 @@ public class AtlasContainer extends CharmContainer {
 
         // player's hotbar
         for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(player, i, 8 + i * 18, 142));
+            this.addSlot(new Slot(player, i, 8 + i * 18, 142) {
+                @Override
+                public boolean canTakeStack(PlayerEntity playerIn) {
+                    return getStack().getItem() != Atlas.ATLAS_ITEM || !Objects.equals(ItemNBTHelper.getUuid(getStack(), AtlasInventory.ID),
+                            ItemNBTHelper.getUuid(atlasInventory.getAtlasItem(), AtlasInventory.ID));
+                }
+            });
         }
     }
 
     public AtlasInventory getAtlasInventory() {
-        return inventory;
+        return atlasInventory;
     }
 }
